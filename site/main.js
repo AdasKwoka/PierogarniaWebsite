f3aaf570e604d5935dd04e316dcb30806ba4b68e
Adam Kwoka <adam.kwoka99@gmail.com>
2020-06-16 11:34:09 +0200

    Improved slider and added menu

--- site/main.js
+++ site/main.js
@@ -1,8 +1,10 @@
 const image = document.querySelector('img.imgSlider');
 const decription = document.querySelector('.motto h1');
+const showMore = document.querySelector('.showMore');
+const menu = document.querySelector('.menu');
 
 let activeElement = 0;
-const timeChange = 4000;
+const timeChange = 6000;
 const images = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg"];
 const descriptions = ["Pierogi naszą pasją", "Lepione z miłością", "Tradycyjne wyroby"];
 
@@ -15,3 +17,17 @@ function changeElement() {
     decription.textContent = descriptions[activeElement];
 }
 setInterval(changeElement, timeChange);
+
+const buttonDescriptions = ["Pokaż więcej", "Pokaż mniej"];
+let counter = 0;
+function showElement() {
+    menu.classList.toggle('activeMenu');
+    showMore.classList.toggle('activeButton');
+    counter++;
+    if (counter == buttonDescriptions.length) {
+        counter = 0;
+    }
+    showMore.textContent = buttonDescriptions[counter];
+}
+
+showMore.addEventListener('click', showElement);
