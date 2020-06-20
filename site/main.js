94e21a8f9a0a14f9e36c60ccd0576d46e0856ce8
Adam Kwoka <adam.kwoka99@gmail.com>
2020-06-20 11:29:04 +0200

    Website without menu

--- site/main.js
+++ site/main.js
@@ -2,6 +2,11 @@ const image = document.querySelector('img.imgSlider');
 const decription = document.querySelector('.motto h1');
 const showMore = document.querySelector('.showMore');
 const menu = document.querySelector('.menu');
+const imgGallery = document.querySelector('.imgGallery');
+const arrowLeft = document.querySelector('.arrow.left');
+const arrowRight = document.querySelector('.arrow.right');
+const leftGalleryPanel = document.querySelector('.galleryWrap');
+const rightGalleryPanel = document.querySelector('.aboutUs');
 
 let activeElement = 0;
 const timeChange = 6000;
@@ -29,5 +34,59 @@ function showElement() {
     }
     showMore.textContent = buttonDescriptions[counter];
 }
-
 showMore.addEventListener('click', showElement);
+
+let active = 0;
+const imagesGallery = ["img/imgGallery1.jpg", "img/imgGallery2.jpg", "img/imgGallery3.jpg", "img/imgGallery4.jpg", "img/imgGallery5.jpg", "img/imgGallery6.jpg"];
+
+
+const changeSlide = () => {
+    active++;
+    if (active == imagesGallery.length) {
+        active = 0;
+    }
+    imgGallery.src = imagesGallery[active];
+}
+
+let indexInterval = setInterval(changeSlide, timeChange);
+
+const clickEventGalleryRight = () => {
+    clearInterval(indexInterval);
+    active++;
+    if (active == imagesGallery.length) {
+        active = 0;
+    }
+    imgGallery.src = imagesGallery[active];
+    indexInterval = setInterval(changeSlide, timeChange);
+}
+const clickEventGalleryLeft = () => {
+    clearInterval(indexInterval);
+    active--;
+    if (active < 0) {
+        active = imagesGallery.length - 1;
+    }
+    imgGallery.src = imagesGallery[active];
+    indexInterval = setInterval(changeSlide, timeChange);
+}
+
+arrowLeft.addEventListener('click', clickEventGalleryLeft);
+arrowRight.addEventListener('click', clickEventGalleryRight);
+
+const showGalleryPanels = () => {
+    const windowHeight = $(window).height();
+    const scrollValue = $(document).scrollTop();
+    const $leftPanel = leftGalleryPanel;
+    const $rightPanel = rightGalleryPanel;
+    const leftFromTop = leftGalleryPanel.offsetTop;
+    const leftHeight = leftGalleryPanel.offsetHeight;
+
+    if (scrollValue > leftFromTop + leftHeight - windowHeight / 2) {
+        leftGalleryPanel.classList.add('active');
+        rightGalleryPanel.classList.add('active');
+    }
+    if (scrollValue < 100) {
+        leftGalleryPanel.classList.remove('active');
+        rightGalleryPanel.classList.remove('active');
+    }
+}
+$(document).on('scroll', showGalleryPanels);
